525a7f1624c37f6a2266c90df71cb22d0734ce91
Fossvold <alekfoss@hotmail.com>
2014-10-24 12:39:27 +0200

    Update Sykkel.java

--- Sykkel.java
+++ Sykkel.java
@@ -1,3 +1,6 @@
+//Gruppemedlemmer: Stian Hvidsten (236619), Aleksander Foss Vold (236608) og Thomas Löfstedt (236612).
+// Informasjonsteknolgi (Kullklassekode: INFORMATIK14HA).
+
 /*Klassen Sykkel skal representere en sykkel i programmet. Klassen skal ha en konstant MAXTID som representerer maksimalt antall timer
 sykkelen kan leies og som skal settes til 3 timer. Hver sykkel skal ha et unikt heltallig id-nummer.
 Dette skal genereres automatisk hver gang et sykkel-objekt opprettes. (Hvordan du kan gjøre det kan du lese om i kapittel 8,
@@ -9,9 +12,9 @@ a) Programmer klassen Sykkel.*/
 public class Sykkel
 {
 
-	public static final int MAXTID = 3600000 * 3;
+	public static final int MAXTID = 10800000;
 	private int sykkelId;
-	private static int nestenummer = 100;
+	private static int nestenummer = 1;
 
 	public Sykkel()
 	{
